08ee53696dd58fe5501e44fb61900f242999dbb2
Gerry Yang <gerry04y@gmail.com>
2025-02-07 17:53:58 +0000

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default async function DashboardPage() {
+  return (
+    <main className="container mx-auto py-10">
+      <h1 className="text-4xl font-bold mb-8">Finance Dashboard</h1>
+    </main>
+  );
+}
